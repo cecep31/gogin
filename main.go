793e0a1bacff2bc6c49afc8a9f1a6ec9592a943b
cecep31 <cecepjanuardi31@gmail.com>
2021-03-01 17:30:54 +0700

    g

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func main() {
 	// connect databases
 	models.Connectdb()
 
-	r.GET("/books", controllers.FindBooks)
-	r.POST("/books", controllers.CreateBook)
-	r.GET("/books/:id", controllers.FindBook)
-	r.PATCH("/books/:id", controllers.UpdateBook)
-	r.DELETE("/books/:id", controllers.DeleteBook)
+	r.GET("/book", controllers.FindBooks)
+	r.POST("/book", controllers.CreateBook)
+	r.GET("/book/:id", controllers.FindBook)
+	r.PATCH("/book/:id", controllers.UpdateBook)
+	r.DELETE("/book/:id", controllers.DeleteBook)
 	r.Run()
 
 }
